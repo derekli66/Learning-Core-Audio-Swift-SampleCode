9ad18809a4642e1eede20bc1f9a94fba4a3ae5b7
Derek Lee <derek.lee@positivegrid.com>
2017-07-23 18:16:13 +0800

    Add CH03 sample code

---
+++ CH03_CAStreamFormatTester-Swift/CH03_CAStreamFormatTester-Swift/main.swift
@@ -0,0 +1,65 @@
+//
+//  main.swift
+//  CH03_CAStreamFormatTester-Swift
+//
+//  Created by LEE CHIEN-MING on 22/07/2017.
+//  Copyright © 2017 derekli66. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+func main()
+{
+    var fileTypeAndFormat = AudioFileTypeAndFormatID(mFileType: kAudioFileAIFFType,
+                                                     mFormatID: kAudioFormatLinearPCM)
+    
+    var audioErr: OSStatus = noErr
+    var infoSize: UInt32   = 0
+    
+    audioErr = AudioFileGetGlobalInfoSize(kAudioFileGlobalInfo_AvailableStreamDescriptionsForFormat,
+                                          UInt32(MemoryLayout.size(ofValue: fileTypeAndFormat)),
+                                          &fileTypeAndFormat,
+                                          &infoSize)
+    
+    if (audioErr != noErr) {
+        let err4cc = CFSwapInt32HostToBig(UInt32(audioErr))
+        debugPrint("Error when getting size info: \(err4cc.toString()!)")
+    }
+    
+    assert(audioErr == noErr)
+    
+    let asbds = UnsafeMutablePointer<AudioStreamBasicDescription>.allocate(capacity: Int(infoSize))
+    asbds.initialize(to: AudioStreamBasicDescription(), count: Int(infoSize))
+    defer {
+        asbds.deinitialize(count: Int(infoSize))
+        asbds.deallocate(capacity: Int(infoSize))
+    }
+    
+    audioErr = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_AvailableStreamDescriptionsForFormat,
+                                      UInt32(MemoryLayout.size(ofValue: fileTypeAndFormat)),
+                                      &fileTypeAndFormat,
+                                      &infoSize,
+                                      asbds)
+    assert(audioErr == noErr)
+    
+    let asbdCount = Int(infoSize) / MemoryLayout<AudioStreamBasicDescription>.size
+    for idx in 0..<asbdCount {
+        let format4cc = CFSwapInt32HostToBig(asbds[idx].mFormatID)
+        debugPrint("\(idx): mFormatId: \(format4cc.toString()!), mFormatFlags: \(asbds[idx].mFormatFlags), mBitsPerChannel: \(asbds[idx].mBitsPerChannel)")
+    }
+}
+
+extension UInt32
+{
+    func toString() -> String?
+    {
+        var value = self
+        let data = Data(bytes: &value, count: MemoryLayout.size(ofValue: value))
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+}
+
+// Perform main function
+main()
+
--- README.md
+++ README.md
@@ -14,6 +14,7 @@ If you would like to share your Swift knowledge to make these sample code better
 
 The chapters I finished in Swift 3.
 
+* Chapter 3
 * Chapter 5
 * Chapter 6
 * Chapter 7
